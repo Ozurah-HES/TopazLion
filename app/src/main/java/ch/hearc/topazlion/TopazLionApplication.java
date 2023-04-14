6409fce838a12c1262ee1d11dd25fc2df2c0f199
Jonas Allemann <jonas.allemann@he-arc.ch>
2023-04-14 13:30:24 +0200

    chore: init project

---
+++ app/src/main/java/ch/hearc/topazlion/TopazLionApplication.java
@@ -0,0 +1,14 @@
+package ch.hearc.topazlion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopazLionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopazLionApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ app/src/test/java/ch/hearc/topazlion/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.hearc.topazlion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopazLionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
